6c59600f6db6b2566f66d5676b8db8f96a88031a
fathur.hidayat <fathur.hidayat@kumparan.com>
2021-09-06 10:37:36 +0700

    initial commit

---
+++ test/fixtures/remove-display-names-from-deps/code.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+const StyledView = styled.div.withConfig({
+  displayName: 'Viewweb__StyledView',
+  componentId: 'sc-1ajfkkc-0',
+})([])
---
+++ test/fixtures/remove-display-names-from-deps/output.js
@@ -0,0 +1,4 @@
+import styled from 'styled-components';
+const StyledView = styled.div.withConfig({
+  componentId: 'sc-1ajfkkc-0'
+})([]);
